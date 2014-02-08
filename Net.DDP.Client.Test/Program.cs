8642335652c882437b16039368608041e0f85687
Paul Duguet <opauldo@gmail.com>
2014-02-08 20:07:26 +0100

    Removed SignalR integration

--- Net.DDP.Client.Test/Program.cs
+++ Net.DDP.Client.Test/Program.cs
@@ -1,19 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Net.DDP.Client;
-using Newtonsoft.Json.Serialization;
-using Newtonsoft.Json.Linq;
-using Net.DDP.SignalR;
+
 namespace Net.DDP.Client.Test
 {
     class Program
     {
         static void Main(string[] args)
         {
-            DDPClientHost host = new DDPClientHost("http://localhost:8081/", "localhost:3000");
-            host.Start();
             Console.ReadLine();
         }
     }
